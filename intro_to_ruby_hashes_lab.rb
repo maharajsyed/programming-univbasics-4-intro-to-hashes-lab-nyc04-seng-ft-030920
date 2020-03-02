d1331d1f0253a4c521cafbe92d47d5fb931fa869
Maharaj Syed <maharajsyed@yahoo.com>
2020-03-02 23:32:06 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,28 +1,46 @@
-def new_hash
-  # return an empty hash
+def new_hash 
+  new_hash={}
+  
+  # Create a method called new_hash that creates and returns a new, empty hash.
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash ={ :hash_id => 1}
+  
+  # Return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  my_pioneer = {:name => "Grace Hopper"}
+  
+  # Create a method called pioneer that returns a hash. This hash should have a key that is a symbol :name and whose value is a string, 'Grace Hopper'.
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  new_id = {:id => 2}
+  
+  # Create a method called id_generator that creates and returns a hash with one key/value pair. The key should be a symbol, :id. For the value, provide a positive integer of your choice.
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash = {key => value}
+  
+  # Create a method called my_hash_creator that takes in two parameters. In this method, create and return a hash with one key/value pair, using the first parameter as the key, and the second parameter as the value.
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash [key]
+  
+  # Create a method call read_from_hash that takes in two parameters. The first parameter is a hash, the second is a key. Used together, they will either produce a value on that hash corresponding to the key, or nil by default. Use these two parameters in conjunction to do just that.
 end
 
 def update_counting_hash(hash, key)
+  if hash[key]
+    hash[key] += 1 
+  else
+    hash[key] = 1 
+  end
+    return hash
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
